bf57dbc49c965f743f4990c5181b1fbe589e0198
Ryder Lee <ryder.lee@mediatek.com>
2021-04-04 15:48:23 +0800

    mt76: mt7615: fix .add_beacon_offload()
    ieee80211_beacon_get_template() returns NULL when beacon state is disabled,
    so beacon_offload cannot be disabled for some devices.
    
    Signed-off-by: Ryder Lee <ryder.lee@mediatek.com>
    Signed-off-by: Felix Fietkau <nbd@nbd.name>

--- mt7615/mcu.c
+++ mt7615/mcu.c
@@ -791,6 +791,9 @@ mt7615_mcu_add_beacon_offload(struct mt7615_dev *dev,
 	};
 	struct sk_buff *skb;
 
+	if (!enable)
+		goto out;
+
 	skb = ieee80211_beacon_get_template(hw, vif, &offs);
 	if (!skb)
 		return -EINVAL;
@@ -820,6 +823,7 @@ mt7615_mcu_add_beacon_offload(struct mt7615_dev *dev,
 	}
 	dev_kfree_skb(skb);
 
+	out:
 	return __mt76_mcu_send_msg(&dev->mt76, MCU_EXT_CMD_BCN_OFFLOAD,
 				   &req, sizeof(req), true);
 }
@@ -1850,8 +1854,8 @@ mt7615_mcu_uni_add_beacon_offload(struct mt7615_dev *dev,
 			__le16 tim_ie_pos;
 			__le16 csa_ie_pos;
 			__le16 bcc_ie_pos;
-			/* 0: enable beacon offload
-			 * 1: disable beacon offload
+			/* 0: disable beacon offload
+			 * 1: enable beacon offload
 			 * 2: update probe respond offload
 			 */
 			u8 enable;
@@ -1873,6 +1877,9 @@ mt7615_mcu_uni_add_beacon_offload(struct mt7615_dev *dev,
 		},
 	};
 	struct sk_buff *skb;
+
+	if (!enable)
+		goto out;
 
 	skb = ieee80211_beacon_get_template(mt76_hw(dev), vif, &offs);
 	if (!skb)
@@ -1898,8 +1905,10 @@ mt7615_mcu_uni_add_beacon_offload(struct mt7615_dev *dev,
 	}
 	dev_kfree_skb(skb);
 
+	out:
 	return __mt76_mcu_send_msg(&dev->mt76, MCU_UNI_CMD_BSS_INFO_UPDATE,
 				   &req, sizeof(req), true);
+}
 }
 
 static int
