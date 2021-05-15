504474ae54121703d581fa8a78475aebc8a5f4fc
Felix Fietkau <nbd@nbd.name>
2021-05-15 08:07:08 +0200

    mt76: fix calling mt76_get_of_eeprom with an offset for pre-cal data
    The device tree partition offset needs to be added to the function argument,
    instead of overwriting it.
    Fixes an issue where wrong EEPROM data was sent to the firmware as
    pre-calibration data on MT7915.
    
    Fixes: 495184ac91bb ("mt76: mt7915: add support for applying pre-calibration data")
    Signed-off-by: Felix Fietkau <nbd@nbd.name>

--- eeprom.c
+++ eeprom.c
@@ -53,7 +53,7 @@ mt76_get_of_eeprom(struct mt76_dev *dev, int len)
 		goto out_put_node;
 	}
 
-	offset = be32_to_cpup(list);
+	offset += be32_to_cpup(list);
 	ret = mtd_read(mtd, offset, len, &retlen, dev->eeprom.data);
 	put_mtd_device(mtd);
 	if (ret)
