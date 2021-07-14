525e7faa92ec5ba828ca0f41d5baba07b16f6961
HiGarfield <HiGarfield@126.com>
2021-07-14 20:13:08 +0800

    mt76: mt7615: fix testmode unknow symbol

--- mt7615/main.c
+++ mt7615/main.c
@@ -1247,8 +1247,6 @@ const struct ieee80211_ops mt7615_ops = {
 	.sched_scan_stop = mt7615_stop_sched_scan,
 	.remain_on_channel = mt7615_remain_on_channel,
 	.cancel_remain_on_channel = mt7615_cancel_remain_on_channel,
-	CFG80211_TESTMODE_CMD(mt76_testmode_cmd)
-	CFG80211_TESTMODE_DUMP(mt76_testmode_dump)
 #ifdef CONFIG_PM
 	.suspend = mt7615_suspend,
 	.resume = mt7615_resume,
