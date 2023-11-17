560261020d94231cf9a75d83a8239775c4dc703c
Evelyn Tsai <evelyn.tsai@mediatek.com>
2023-11-18 07:36:45 +0800

    wifi: mt76: ACS channel time too long on duty channel
    Issue:
    There's a chance that the channel time for duty channel is zero in ACS
    scan.
    
    Root cause:
    The chan_stat may be reset when restore to duty channel.
    Mac80211 will notify to hostapd when scan done and then restore to duty
    channel.
    And mt76 will clear scan flag after restore done.
    If hostapd get the chan_stat before channel_restore, will get the
    correct channel time;
    If hostapd get the chan_stat after channel_restore, will get zero
    channel time;
    
    Solution:
    When channel switch, will check the mac80211 scan state but not the mt76 scan flag.
    Mac80211 scan state will be set in scanning, and will be reset after
    scan done and before restore to duty channel.

--- mac80211.c
+++ mac80211.c
@@ -610,6 +610,7 @@ void mt76_set_channel(struct mt76_phy *phy)
 	struct cfg80211_chan_def *chandef = &hw->conf.chandef;
 	bool offchannel = hw->conf.flags & IEEE80211_CONF_OFFCHANNEL;
 	int timeout = HZ / 5;
+	bool was_scanning = test_bit(MT76_SCANNING, &phy->state);
 
 	wait_event_timeout(dev->tx_wait, !mt76_has_tx_pending(phy), timeout);
 	mt76_update_survey(dev);
@@ -624,7 +625,7 @@ void mt76_set_channel(struct mt76_phy *phy)
 	if (!offchannel)
 		phy->main_chan = chandef->chan;
 
-	if (chandef->chan != phy->main_chan)
+	if (chandef->chan != phy->main_chan || was_scanning)
 		memset(phy->chan_state, 0, sizeof(*phy->chan_state));
 }
 EXPORT_SYMBOL_GPL(mt76_set_channel);
